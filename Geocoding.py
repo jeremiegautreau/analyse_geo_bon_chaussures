6363f8048b0b91293086bf3b3bcae2e6fbd0b4c6
pierregautreau44 <gautreau@buffalo.edu>
2023-04-01 11:50:21 -0500

    refactored the geocoding function

--- Geocoding.py
+++ Geocoding.py
@@ -1,39 +1,40 @@
-
 import requests
 import pandas as pd
 from io import StringIO
 
 
-#path_add = 'C://Users//jerem//Bon chaussures//analyse_geo_add.csv'
-
-
-def geocoding(path_add, col_add):
-
-    url = 'https://api-adresse.data.gouv.fr/search/csv/'
-
-    files = [
-        ('data', open(path_add, 'rb')),
-        ('columns', (None, col_add)),
+DEFAULT_REQUEST = [
         ('result_columns', (None, 'latitude')),
         ('result_columns', (None, 'longitude')),
         ('result_columns', (None, 'result_city')),
         ('result_columns', (None, 'result_citycode')),
     ]
 
+
+def format_geocoded_data(geocoded_data):
+    df = pd.read_csv(StringIO(geocoded_data.decode('utf-8')), sep=';')
+    df['Utilise'].fillna(0, inplace=True)
+    df.dropna(subset=['latitude', 'longitude'], inplace=True)
+    return df
+
+
+def geocoding(path_add, col_add, files=DEFAULT_REQUEST):
+    files.append(
+        ('data', open(path_add, 'rb')),
+        ('columns', (None, col_add))
+    )
+    url = 'https://api-adresse.data.gouv.fr/search/csv/'
     response = requests.post(url, files=files)
 
     if response.status_code == 200:
         status = 'Request successful'
-
         geocoded = response.content
-
-        df = pd.read_csv(StringIO(geocoded.decode('utf-8')), sep=';')
-        df['Utilise'].fillna(0, inplace=True)
-        df.dropna(subset=['latitude', 'longitude'], inplace=True)
-
+        df = format_geocoded_data(geocoded)
         return df, print(status)
-
     else:
         status = 'Error during request to API: '+str(response.status_code)
-
         return print(status)
+
+
+if __name__ == '__main__':
+    pass
